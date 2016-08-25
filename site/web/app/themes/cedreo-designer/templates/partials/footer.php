cc273185c82f7bdcdf5d6acaf7d7af5b860441cc
wabdsgn <wab2com@gmail.com>
2016-08-25 11:14:20 +0200

    footer

--- site/web/app/themes/cedreo-designer/templates/partials/footer.php
+++ site/web/app/themes/cedreo-designer/templates/partials/footer.php
@@ -1,5 +1,27 @@
-<footer class="content-info">
-  <div class="container">
+<footer class="mainfooter">
+  <div class="widget--area">
+    <div class="widget">
+      <h3>My Sketcher</h3>
+      <nav class="nav-footer">
+        <ul class="no-bullet">
+          <li><a href="<?php bloginfo('url'); ?>/qui-sommes-nous">Qui sommes-nous ?</a></li>
+          <li><a href="<?php bloginfo('url'); ?>/tutoriels">Tutoriels</a></li>
+          <li><a href="">Forum</a></li>
+          <li><a href="<?php bloginfo('url'); ?>/partenaires">Devenez partenaires</a></li>
+          <li><a href="<?php bloginfo('url'); ?>/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
+  </div>
+  <div class="ours text-center">
+    <p><a href="#">Conditions d'utilisation</a> - <a href="#">Mentions légales</a> - &copy; Copyright 2016 Mysketcher</p>
+    <ul class="reseaux--list no-bullet">
+      <li class="reseaux--item">twitter</li>
+      <li class="reseaux--item">facebook</li>
+      <li class="reseaux--item">google-plus</li>
+      <li class="reseaux--item">youtube</li>
+      <li class="reseaux--item">pinterest</li>
+    </ul>
   </div>
 </footer>
