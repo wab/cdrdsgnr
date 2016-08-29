dc908cd53a8f64e28e3e92e7ebfbabd975e69f98
wabdsgn <wab2com@gmail.com>
2016-08-29 17:26:32 +0200

    banner step, icon alignment & pinto title

--- site/web/app/themes/cedreo-designer/templates/page-accueil.php
+++ site/web/app/themes/cedreo-designer/templates/page-accueil.php
@@ -8,13 +8,25 @@
   <header class="banner">
     <h1 class="site--title"><span class="textcolor">Plans 3D</span>, rendus intérieurs et extérieurs en haute qualité, <span class="textcolor">simplement et rapidement</span></h1>
     <ol class="step--list">
-      <li class="step--item">Dessiner votre projet<svg class="icon icon-step"><use xlink:href="#icon-step"></use></svg></li>
-      <li class="step--item">Meubler et décorer<svg class="icon icon-step"><use xlink:href="#icon-step"></use></svg></li>
-      <li class="step--item">Obtenez des rendus étonnants<svg class="icon icon-step"><use xlink:href="#icon-step"></use></svg></li>
+      <li class="step--item">
+        <p><img src="https://unsplash.it/g/300/200/?image=10" alt=""></p>
+        <p class="step--title">Dessiner votre projet</p>
+        <svg class="icon-step"><use xlink:href="#icon-step"></use></svg>
+      </li>
+      <li class="step--item">
+        <p><img src="https://unsplash.it/g/300/200/?image=10" alt=""></p>
+        <p class="step--title">Meubler et décorer</p>
+        <svg class="icon-step"><use xlink:href="#icon-step"></use></svg>
+      </li>
+      <li class="step--item">
+        <p><img src="https://unsplash.it/g/300/200/?image=10" alt=""></p>
+        <p class="step--title">Obtenez des rendus étonnants</p>
+        <svg class="icon-step"><use xlink:href="#icon-step"></use></svg>
+      </li>
     </ol>
     <div class="cta">
-      <a href="#" class="button alert try"><svg class="icon icon-download"><use xlink:href="#icon-download"></use></svg> Essai gratuit</a>
-      <span class="picto"><svg class="icon icon-timer"><use xlink:href="#icon-timer"></use></svg></span>
+      <a href="#" class="button alert try"><svg class="icon-download"><use xlink:href="#icon-download"></use></svg> Essai gratuit</a>
+      <span class="picto"><svg class="icon-timer"><use xlink:href="#icon-timer"></use></svg></span>
       <a href="#" class="button">présentation vidéo</a>
     </div>
   </header>
@@ -23,7 +35,7 @@
   <section class="demonstration section">
     <div class="demonstration--wrapper">
       <div class="demonstration--column text-center">
-        <h2 class="demonstration--title">Ces images peuvent êtes faites <strong>en moins de <span class="textcolor">20 minutes</span></strong><br><span class="textcolor">vous voulez savoir comment ?<br><svg class="icon icon-arrow"><use xlink:href="#icon-arrow"></use></svg></span></h2>
+        <h2 class="demonstration--title">Ces images peuvent êtes faites <strong>en moins de <span class="textcolor">20 minutes</span></strong><br><span class="textcolor">vous voulez savoir comment ?<br><svg class="icon-arrow"><use xlink:href="#icon-arrow"></use></svg></span></h2>
       </div>
       <div class="demonstration--column">
         <div class="owl-carousel owl-theme">
@@ -82,29 +94,31 @@
   </section>
 
   <section class="section step">
+    <span class="picto"><svg class="icon-cog"><title>Les étapes</title><use xlink:href="#icon-cog"></use></svg></span>
     <ol class="step--list">
       <li class="step--item">
         <img src="" alt="plan">
         <h2 class="step--title">Dessinez vos plans facilement et rapidement</h2>
         <p>Créez votre plan dans un éditeur 2D clair en intégrant des cloisonnements, des mezzanines, des pentes de toits et en ajoutant des ouvertures personnalisables.</p>
-        <svg class="icon icon-step"><use xlink:href="#icon-step"></use></svg>
+        <svg class="icon-step"><use xlink:href="#icon-step"></use></svg>
       </li>
       <li class="step--item">
         <img src="" alt="cananpé">
         <h2 class="step--title">Concevez votre intérieur en quelques clics</h2>
         <p>Votre plan est prêt, vous pouvez le modifier avec des milliers de produits et matériaux de notre catalogue et génériques.</p>
-        <svg class="icon icon-step"><use xlink:href="#icon-step"></use></svg>
+        <svg class="icon-step"><use xlink:href="#icon-step"></use></svg>
       </li>
       <li class="step--item">
         <img src="" alt="rendu 3D">
         <h2 class="step--title">Générez des photos 3D de haute qualité</h2>
         <p>Obtenez une qualité de rendu stupéfiante en moins de 1 min. Choisissez juste votre point de vue et votre environnement; nous calculons votre photo en moins de 1 min!</p>
-        <svg class="icon icon-step"><use xlink:href="#icon-step"></use></svg>
+        <svg class="icon-step"><use xlink:href="#icon-step"></use></svg>
       </li>
     </ol>
   </section>
 
   <section class="section help">
+    <span class="picto"><svg class="icon-cog"><title>Les étapes</title><use xlink:href="#icon-cog"></use></svg></span>
     <div class="row">
       <div class="columns medium-6">
         <div class="images-grid">
@@ -117,33 +131,34 @@
       <div class="columns medium-6">
         <h2>Besoin d'aide pour réaliser vos plans ?</h2>
         <ul class="menu vertical">
-          <li><a href="#"><svg class="icon icon-users"><use xlink:href="#icon-users"></use></svg>Accédez au forum</a></li>
-          <li><a href="#"><svg class="icon icon-tutos"><use xlink:href="#icon-tutos"></use></svg>Consultez les tutoriels</a></li>
-          <li><a href="#"><svg class="icon icon-envelop"><use xlink:href="#icon-envelop"></use></svg>Contactez-nous</a></li>
+          <li><a href="#"><svg class="icon-users"><use xlink:href="#icon-users"></use></svg>Accédez au forum</a></li>
+          <li><a href="#"><svg class="icon-tutos"><use xlink:href="#icon-tutos"></use></svg>Consultez les tutoriels</a></li>
+          <li><a href="#"><svg class="icon-envelop"><use xlink:href="#icon-envelop"></use></svg>Contactez-nous</a></li>
         </ul>
       </div>
     </div>
   </section>
 
   <section class="section why">
+    <span class="picto"><svg class="icon-cog"><title>Les étapes</title><use xlink:href="#icon-cog"></use></svg></span>
     <h2 class="text-center">Pourquoi devriez-vous utiliser My Sketcher ?</h2>
     <div class="row">
       <div class="columns medium-6">
         <ul class="why--list">
-          <li class="why--item"><svg class="icon icon-check"><use xlink:href="#icon-check"></use></svg> La façon la plus facile et la plus rapide de produire des plans d'architecte et des rendus intérieurs 3D</li>
-          <li class="why--item"><svg class="icon icon-check"><use xlink:href="#icon-check"></use></svg> Aucune formation nécessaire</li>
-          <li class="why--item"><svg class="icon icon-check"><use xlink:href="#icon-check"></use></svg> Pas besoin de télécharger et d'installer quoi que ce soit</li>
-          <li class="why--item"><svg class="icon icon-check"><use xlink:href="#icon-check"></use></svg> Options de rendu avancées pour un travail professionnel</li>
-          <li class="why--item"><svg class="icon icon-check"><use xlink:href="#icon-check"></use></svg> Un résultat professionnel en quelques minutes</li>
+          <li class="why--item"><svg class="icon-check"><use xlink:href="#icon-check"></use></svg> La façon la plus facile et la plus rapide de produire des plans d'architecte et des rendus intérieurs 3D</li>
+          <li class="why--item"><svg class="icon-check"><use xlink:href="#icon-check"></use></svg> Aucune formation nécessaire</li>
+          <li class="why--item"><svg class="icon-check"><use xlink:href="#icon-check"></use></svg> Pas besoin de télécharger et d'installer quoi que ce soit</li>
+          <li class="why--item"><svg class="icon-check"><use xlink:href="#icon-check"></use></svg> Options de rendu avancées pour un travail professionnel</li>
+          <li class="why--item"><svg class="icon-check"><use xlink:href="#icon-check"></use></svg> Un résultat professionnel en quelques minutes</li>
         </ul>
       </div>
       <div class="columns medium-6">
 
         <ul class="why--list">
-          <li class="why--item"><svg class="icon icon-check"><use xlink:href="#icon-check"></use></svg> La meilleure qualité de rendu au monde pour un logiciel de plan</li>
-          <li class="why--item"><svg class="icon icon-check"><use xlink:href="#icon-check"></use></svg> + de 1000 articles et matériaux personnalisables de haute qualité pour personnaliser votre intérieur</li>
-          <li class="why--item"><svg class="icon icon-check"><use xlink:href="#icon-check"></use></svg> Rendus intérieurs et extérieurs</li>
-          <li class="why--item"><svg class="icon icon-check"><use xlink:href="#icon-check"></use></svg> Payer ce que vous utilisez</li>
+          <li class="why--item"><svg class="icon-check"><use xlink:href="#icon-check"></use></svg> La meilleure qualité de rendu au monde pour un logiciel de plan</li>
+          <li class="why--item"><svg class="icon-check"><use xlink:href="#icon-check"></use></svg> + de 1000 articles et matériaux personnalisables de haute qualité pour personnaliser votre intérieur</li>
+          <li class="why--item"><svg class="icon-check"><use xlink:href="#icon-check"></use></svg> Rendus intérieurs et extérieurs</li>
+          <li class="why--item"><svg class="icon-check"><use xlink:href="#icon-check"></use></svg> Payer ce que vous utilisez</li>
         </ul>
       </div>
     </div>
