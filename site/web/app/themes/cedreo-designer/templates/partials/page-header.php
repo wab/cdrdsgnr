430d26004ab4881dc9e804a10ac9250e36a4d577
wabdsgn <wab2com@gmail.com>
2016-09-21 22:26:06 +0200

    add breadcrumb majs lg

--- site/web/app/themes/cedreo-designer/templates/partials/page-header.php
+++ site/web/app/themes/cedreo-designer/templates/partials/page-header.php
@@ -1,5 +1,10 @@
 <header class="page-header text-center banner">
   <div class="wrapper">
     <h1><?= App\title(); ?></h1>
+    <?php if(function_exists('bcn_display')):?>
+    <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
+      <?php bcn_display(); ?>
+    </div>
+    <?php endif; ?>
   </div>
 </header>
