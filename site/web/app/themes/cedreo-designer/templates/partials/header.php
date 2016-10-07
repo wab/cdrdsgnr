b1119e2a348466845ebf9a5c92f5de444eb9c1f6
wabdsgn <wab2com@gmail.com>
2016-10-07 14:46:31 +0200

    header

--- site/web/app/themes/cedreo-designer/templates/partials/header.php
+++ site/web/app/themes/cedreo-designer/templates/partials/header.php
@@ -1,13 +1,15 @@
 <header class="mainheader">
-  <a class="brand" href="<?= esc_url(home_url('/')); ?>">
-    <?php get_template_part('partials/logo'); ?>
-  </a>
-  <nav class="main--navigation">
-    <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'main--navigation--menu dropdown vertical medium-horizontal menu']);
-      endif;
-    ?>
-  </nav>
+  <div class="mainheader--wrapper">
+    <a class="brand" href="<?= esc_url(home_url('/')); ?>">
+      <?php get_template_part('partials/logo'); ?>
+    </a>
+    <nav class="main--navigation">
+      <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'main--navigation--menu dropdown vertical medium-horizontal menu']);
+        endif;
+      ?>
+    </nav>
+  </div>
 </header>
 
