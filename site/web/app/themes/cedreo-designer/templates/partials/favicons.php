03a7620d11f97cf4c3f117e0c05e3cd7ff3894d9
wabdsgn <wab2com@gmail.com>
2016-10-11 15:27:19 +0200

    favicons

---
+++ site/web/app/themes/cedreo-designer/templates/partials/favicons.php
@@ -0,0 +1,9 @@
+
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/favicon-16x16.png" sizes="16x16">
+<!-- <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/manifest.json"> -->
+<link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/safari-pinned-tab.svg" color="#00b7aa">
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/favicon.ico">
+<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/dist/images/favicons/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
--- site/web/app/themes/cedreo-designer/templates/partials/head.php
+++ site/web/app/themes/cedreo-designer/templates/partials/head.php
@@ -2,10 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <style>
-    .icon {
-      display: none;
-    }
-  </style>
+  <?php get_template_part('partials/favicons'); ?>
   <?php wp_head(); ?>
 </head>
