65d93603499824cf5d559157546825acffbf37f1
wabdsgn <wab2com@gmail.com>
2016-10-21 11:28:17 +0200

    static nav desktop

--- site/web/app/themes/cedreo-designer/assets/scripts/main.js
+++ site/web/app/themes/cedreo-designer/assets/scripts/main.js
@@ -5,14 +5,16 @@ import 'jquery';
 import 'svgxuse';
 import 'foundation-sites/js/foundation.core';
 import 'foundation-sites/js/foundation.util.mediaQuery';
+import 'foundation-sites/js/foundation.util.keyboard';
+import 'foundation-sites/js/foundation.util.box';
+import 'foundation-sites/js/foundation.util.nest';
+import 'foundation-sites/js/foundation.util.triggers';
+import 'foundation-sites/js/foundation.util.motion';
+import 'foundation-sites/js/foundation.util.timerAndImageLoader';
 import 'foundation-sites/js/foundation.responsiveToggle';
-import 'foundation-sites/js/foundation.util.keyboard.js';
-import 'foundation-sites/js/foundation.util.box.js';
-import 'foundation-sites/js/foundation.util.nest.js';
-import 'foundation-sites/js/foundation.util.triggers.js';
-import 'foundation-sites/js/foundation.util.motion.js';
-import 'foundation-sites/js/foundation.reveal.js';
-import 'foundation-sites/js/foundation.dropdown.js';
+import 'foundation-sites/js/foundation.interchange';
+import 'foundation-sites/js/foundation.dropdown';
+import 'foundation-sites/js/foundation.reveal';
 
 // import local dependencies
 import Router from './util/router';
--- site/web/app/themes/cedreo-designer/templates/partials/navigation-desktop.php
+++ site/web/app/themes/cedreo-designer/templates/partials/navigation-desktop.php
@@ -12,11 +12,11 @@
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione eveniet culpa exercitationem voluptas incidunt consectetur minus, aliquid perspiciatis beatae cum aut, molestias id laudantium, deleniti nam quidem velit delectus cumque?
         </div>
       </li>
-      <li class="menu-item menu-galerie"><a href="//localhost:3000/galerie/">Galerie</a></li>
-      <li class="menu-item menu-tarifs"><a href="//localhost:3000/tarifs/">Tarifs</a></li>
-      <li class="menu-item menu-support"><a href="http://help.cedreo-designer.com">Support</a></li>
-      <li class="menu-item menu-blog"><a href="//localhost:3000/blog/">Blog</a></li>
-      <li class="link-login menu-item menu-connexion"><a href="http://app.cedreo-designer.com/fr/login">Connexion</a></li>
-      <li class="lang-item lang-item-6 lang-item-en menu-item menu-english"><a href="http://cedreo-designer.com.dev/" hreflang="en-US" lang="en-US"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAALCAIAAAD5gJpuAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAHzSURBVHjaYkxOP8IAB//+Mfz7w8Dwi4HhP5CcJb/n/7evb16/APL/gRFQDiAAw3JuAgAIBEDQ/iswEERjGzBQLEru97ll0g0+3HvqMn1SpqlqGsZMsZsIe0SICA5gt5a/AGIEarCPtFh+6N/ffwxA9OvP/7//QYwff/6fZahmePeB4dNHhi+fGb59Y4zyvHHmCEAAAW3YDzQYaJJ93a+vX79aVf58//69fvEPlpIfnz59+vDhw7t37968efP3b/SXL59OnjwIEEAsDP+YgY53b2b89++/awvLn98MDi2cVxl+/vl6mituCtBghi9f/v/48e/XL86krj9XzwEEEENy8g6gu22rfn78+NGs5Ofr16+ZC58+fvyYwX8rxOxXr169fPny+fPn1//93bJlBUAAsQADZMEBxj9/GBxb2P/9+S/R8u3vzxuyaX8ZHv3j8/YGms3w8ycQARmi2eE37t4ACCDGR4/uSkrKAS35B3TT////wADOgLOBIaXIyjBlwxKAAGKRXjCB0SOEaeu+/y9fMnz4AHQxCP348R/o+l+//sMZQBNLEvif3AcIIMZbty7Ly6t9ZmXl+fXj/38GoHH/UcGfP79//BBiYHjy9+8/oUkNAAHEwt1V/vI/KBY/QSISFqM/GBg+MzB8A6PfYC5EFiDAABqgW776MP0rAAAAAElFTkSuQmCC" title="English" alt="English" /><span style="margin-left:0.3em;">English</span></a></li>
+      <li class="menu-item menu-galerie"><a href="<?php echo get_permalink(9); ?>"><?php echo get_the_title(9); ?></a></li>
+      <li class="menu-item menu-tarifs"><a href="<?php echo get_permalink(15); ?>"><?php echo get_the_title(15); ?></a></li>
+      <li class="menu-item menu-support"><a href="http://help.cedreo-designer.com"><?php echo __('Support', 'cedreo') ?></a></li>
+      <li class="menu-item menu-blog"><a href="<?php echo get_permalink(17); ?>"><?php echo get_the_title(17); ?></a></li>
+      <li class="link-login menu-item menu-connexion"><a href="http://app.cedreo-designer.com/fr/login"><?php echo __('Connexion', 'cedreo') ?></a></li>
+      <?php pll_the_languages(array('show_flags'=>1,'show_names'=>0, 'hide_current'=> 1));?>
     </ul>
 </nav>
