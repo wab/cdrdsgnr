090ebae33d22d0a7dab4093be75b93a0c8d8d1d4
wabdsgn <wab2com@gmail.com>
2016-11-17 21:33:31 +0100

    font size gallery excerpt title

--- site/web/app/themes/cedreo-designer/templates/partials/excerpt-gallery.php
+++ site/web/app/themes/cedreo-designer/templates/partials/excerpt-gallery.php
@@ -8,7 +8,7 @@ $terms = wp_get_object_terms( $post->ID,  'gallerytag' );
     <img class="gallery--item--thumb" src="<?php echo $image['sizes']['gallery']; ?>" alt="<?php echo $image['alt']; ?>" />
     <button class="gallery--item--overlay" data-src="<?php echo $image['url']; ?>" data-sub-html=".gallery--item--caption" data-pinterest-text="<?php the_title(); ?> - image produite avec <?php bloginfo('title'); ?>" data-tweet-text="<?php the_title(); ?> - image produite avec <?php bloginfo('title'); ?>" data-facebook-share-url="<?php echo $image['url']; ?>" data-twitter-share-url="<?php echo $image['url']; ?>" data-googleplus-share-url="<?php echo $image['url']; ?>" data-pinterest-share-url="<?php echo $image['url']; ?>">
       <div class="gallery--item--overlay--container text-center">
-          <h2><?php the_title(); ?></h2>
+          <h2 class="gallery--item--overlay--title"><?php the_title(); ?></h2>
           <?php
             if ( ! empty( $terms ) ) {
               if ( ! is_wp_error( $terms ) ) {
